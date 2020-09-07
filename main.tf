9b90a0ddc0a5ffb51d15902160b53885091ae87a
ShivaniAgrawal049 <40591882+ShivaniAgrawal049@users.noreply.github.com>
2020-09-07 08:52:01 +0530

    Update and rename ec2.tf to main.tf

--- main.tf
+++ main.tf
@@ -63,12 +63,3 @@ resource "aws_instance" "TestInstance1" {
 
   }
 
-#outputs
-
-output "TestInstance1_pub_ip" {
-    value = "${aws_instance.TestInstance1.0.public_ip}"
-}
-
-output "TestInstance1_id" {
-    value = "${aws_instance.TestInstance1.0.id}"
-}
