0b59d6d2246b69bb45f771584fb82e5e7fdb72d9
ShivaniAgrawal049 <40591882+ShivaniAgrawal049@users.noreply.github.com>
2020-09-07 08:49:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,51 +6,23 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-#Create postgresql DB
-resource "aws_db_instance" "postgresql" {
-  allocated_storage               = var.allocated_storage
-  engine                          = "postgres"
-  engine_version                  = var.engine_version
-  identifier                      = var.database_identifier
-  snapshot_identifier             = var.snapshot_identifier
-  instance_class                  = var.instance_type
-  storage_type                    = var.storage_type
-  iops                            = var.iops
-  name                            = var.database_name
-  password                        = var.database_password
-  username                        = var.database_username
-  backup_retention_period         = var.backup_retention_period
-  backup_window                   = var.backup_window
-  maintenance_window              = var.maintenance_window
-  auto_minor_version_upgrade      = var.auto_minor_version_upgrade
-  final_snapshot_identifier       = var.final_snapshot_identifier
-  skip_final_snapshot             = var.skip_final_snapshot
-  copy_tags_to_snapshot           = var.copy_tags_to_snapshot
-  multi_az                        = var.multi_availability_zone
-  port                            = var.database_port
-  vpc_security_group_ids          = [aws_security_group.postgresql.id]
-  db_subnet_group_name            = var.subnet_group
-  parameter_group_name            = var.parameter_group
-  storage_encrypted               = var.storage_encrypted
-  monitoring_interval             = var.monitoring_interval
-  monitoring_role_arn             = var.monitoring_interval > 0 ? aws_iam_role.enhanced_monitoring.arn : ""
-  deletion_protection             = var.deletion_protection
-  enabled_cloudwatch_logs_exports = var.cloudwatch_logs_exports
+data "aws_vpc" "default" {
+  default = true
+}
 
-  tags = merge(
-    {
-      Name        = "DatabaseServer",
-      Project     = var.project,
-      Environment = var.environment
-    },
-    var.tags
-  )
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  name   = "default"
 }
 
 #Create EC2 instance
 resource "aws_instance" "TestInstance1" {
   ami             = "ami-09052aa9bc337c78d"
-  instance_type   = "${var.instance_type}"
+  instance_type   = "${var.ec_instance_type}"
   count = 1
   key_name = "awskey1"
   tags = {
