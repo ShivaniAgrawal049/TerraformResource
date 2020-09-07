13399e8f778e16299fa9111860ffdd51ee15489d
ShivaniAgrawal049 <40591882+ShivaniAgrawal049@users.noreply.github.com>
2020-09-07 08:06:31 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    type = "string"
+    default = "AKIA45MIFH5SGG6Q6WOA"
+}
+
+variable "secret_key" {
+    type = "string"
+    default = "r6GtHL8Cjm3ml/iak4PirNjiBU1NHJChudAK+gr9"
+}
+
+variable "region" {
+    type = "string"
+    default = "ap-south-1"
+}
+
+variable "instance_type" {
+    type = "string"
+    default = "t2.micro"
+}
