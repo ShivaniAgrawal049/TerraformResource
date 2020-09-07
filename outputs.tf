ca13b5389566942de5f1f3fd60da4007bbd632e1
ShivaniAgrawal049 <40591882+ShivaniAgrawal049@users.noreply.github.com>
2020-09-07 09:41:02 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,25 @@ output "TestInstance1_pub_ip" {
 output "TestInstance1_id" {
     value = "${aws_instance.TestInstance1.0.id}"
 }
+
+output "hostname" {
+  value       = aws_db_instance.postgresql.address
+  description = "Public DNS name of database instance"
+}
+
+output "id" {
+  value       = aws_db_instance.postgresql.id
+  description = "The database instance ID"
+}
+
+output "port" {
+  value       = aws_db_instance.postgresql.port
+  description = "Port of database instance"
+}
+
+output "endpoint" {
+  value       = aws_db_instance.postgresql.endpoint
+  description = "Public DNS name and port separated by a colon"
+}
+
+
